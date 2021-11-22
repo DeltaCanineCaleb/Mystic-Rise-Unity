641cdc15d2ccd53e33d43490d392bb2add9763b5
DeltaCanineCaleb <89848601+DeltaCanineCaleb@users.noreply.github.com>
2021-11-22 08:04:06 -0500

    Small battle system progress

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    public string characterName;
+    public int characterLevel;
+
+    public int attack;
+    // public int defense;
+    // public int speed;
+    public int currentHP;
+    public int maxHP;
+}
