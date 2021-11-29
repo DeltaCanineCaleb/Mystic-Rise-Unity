64981c5167f57bc2759812edb510e3a27bb2d4f0
DeltaCanineCaleb <89848601+DeltaCanineCaleb@users.noreply.github.com>
2021-11-29 08:27:42 -0500

    Attack button works (once)

--- Assets/Scripts/BattleScript.cs
+++ Assets/Scripts/BattleScript.cs
@@ -1,17 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public enum CurrentBattleState { START , YOUR_TURN, AWAITING_TURN, CPU_TURN, END}
 
 public class BattleScript : MonoBehaviour
 {
     public CurrentBattleState battleState;
+
     public GameObject characterPrefab;
     public Transform[] battleStations;
     public GameObject player;
+
     PlayerState stateEnum;
     PlayerState.CurrentPlayerState playerState;
+
+    public Text battleText;
+    public GameObject panelOfButtons;
+    public Button attackButton;
 
     void Start() {
         stateEnum = GameObject.Find("GameManager").GetComponent<PlayerState>();
@@ -33,14 +40,35 @@ public class BattleScript : MonoBehaviour
         GameObject enemyOpponent = Instantiate(characterPrefab, battleStations[1].transform.position, Quaternion.identity);
         // determine player turn order, based on speeds*
         // player goes first for now
-        GameObject[] turnOrder = new GameObject[] {player, enemyOpponent};
+        List<GameObject> turnOrder = new List<GameObject>();
+        turnOrder.Add(player);
+        turnOrder.Add(enemyOpponent);
+        List<GameObject> leftSide = new List<GameObject>();
+        leftSide.Add(player);
+        List<GameObject> rightSide = new List<GameObject>();
+        rightSide.Add(enemyOpponent);
+        List<GameObject> battleCharacters = new List<GameObject>();
+        battleCharacters.AddRange(leftSide);
+        battleCharacters.AddRange(rightSide);
+        // choose funny battle start text depending on the enemy
+        battleText.text = battleCharacters[1].GetComponent<Character>().characterName + " attacks!";
         // start the battle loop using the turn order
+        StartCoroutine(PlayerTurn(turnOrder, battleCharacters));
     }
 
-    void BattleLoop()
+    IEnumerator PlayerTurn(List<GameObject> turnOrder, List<GameObject> battleCharacters)
     {
-        // if its your turn, YOUR_TURN state (recieve input from this system)
-        // if its a friend's turn, AWAITING_TURN state (recieve input from other systems) (wait for multiplayer implementation)
-        // if its a CPU's turn, CPU_turn, resolve what happens (multiplayer note: make sure its the same on all sides) and move forward
+        panelOfButtons.SetActive(true);
+        var waitForButton = new WaitForUIButtons(attackButton);
+        yield return waitForButton.Reset();
+        if (waitForButton.PressedButton == attackButton)
+        {
+            GameObject target = battleCharacters[1];
+            battleText.text = turnOrder[0].GetComponent<Character>().characterName + " attacked for " + turnOrder[0].GetComponent<Character>().attack + " damage!";
+            target.GetComponent<Character>().currentHP -= battleCharacters[0].GetComponent<Character>().attack;
+            panelOfButtons.SetActive(false);
+            // check for if anyone died
+            // move turnOrder
+        }
     }
 }
