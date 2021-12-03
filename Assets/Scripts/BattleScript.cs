95246df36e33dc3f454500741e7a6ea4e098ec92
DeltaCanineCaleb <89848601+DeltaCanineCaleb@users.noreply.github.com>
2021-12-03 08:24:32 -0500

    Run button functionality, battle trigger fully works

--- Assets/Scripts/BattleScript.cs
+++ Assets/Scripts/BattleScript.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using Random = System.Random;
 
 public enum CurrentBattleState { START , YOUR_TURN, AWAITING_TURN, CPU_TURN, END}
 
@@ -19,6 +20,13 @@ public class BattleScript : MonoBehaviour
     public Text battleText;
     public GameObject panelOfButtons;
     public Button attackButton;
+    public Button itemsButton;
+    public Button skillsButton;
+    public Button runButton;
+    public Transform cameraTransform;
+
+    public int runChance;
+    public float waitTime;
 
     void Start() {
         stateEnum = GameObject.Find("GameManager").GetComponent<PlayerState>();
@@ -32,6 +40,12 @@ public class BattleScript : MonoBehaviour
             BattleStart(null);
         }
     }
+
+    int RandomNumber(int min, int max)
+    {
+        Random random = new Random(); 
+        return random.Next(min, max);
+    }   
     
     public void BattleTrigger(GameObject enemy)
     {
@@ -48,6 +62,9 @@ public class BattleScript : MonoBehaviour
             enemyOpponent = Instantiate(characterPrefab, battleStations[1].transform.position, Quaternion.identity);
         } else {
             enemyOpponent = enemy;
+            cameraTransform.position = new Vector3(enemy.transform.position.x, enemy.transform.position.y, -10f);
+            enemy.transform.position = battleStations[1].transform.position;
+            enemy.GetComponent<Rigidbody2D>().velocity = new Vector2(0f, 0f);
         }
         // determine player turn order, based on speeds*
         // player goes first for now
@@ -62,38 +79,62 @@ public class BattleScript : MonoBehaviour
         StartCoroutine(PlayerTurn(turnOrder, battleCharacters));
     }
 
-    void checkDeath(List<GameObject> battleCharacters) 
+    bool checkDeath(List<GameObject> battleCharacters) 
     {
         if (battleCharacters[0].GetComponent<Character>().currentHP <= 0) {
             Destroy(battleCharacters[0]);
             stateEnum.state = PlayerState.CurrentPlayerState.OVERWORLD;
             StopAllCoroutines();
+            return true;
         } else if (battleCharacters[1].GetComponent<Character>().currentHP <= 0) {
             Destroy(battleCharacters[1]);
             stateEnum.state = PlayerState.CurrentPlayerState.OVERWORLD;
             StopAllCoroutines();
+            return true;
+        } else {
+            return false;
         }
     }
 
     IEnumerator PlayerTurn(List<GameObject> turnOrder, List<GameObject> battleCharacters)
     {
+        if (battleText.text.Contains("damage")) {
+            battleText.text = "What will you do?";
+        }
         panelOfButtons.SetActive(true);
-        var waitForButton = new WaitForUIButtons(attackButton);
+        var waitForButton = new WaitForUIButtons(attackButton, runButton);
         yield return waitForButton.Reset();
-        if (waitForButton.PressedButton == attackButton)
-        {
+        if (waitForButton.PressedButton == attackButton) {
             GameObject target = battleCharacters[1];
             battleText.text = turnOrder[0].GetComponent<Character>().characterName + " attacked for " + turnOrder[0].GetComponent<Character>().attack + " damage!";
             target.GetComponent<Character>().currentHP -= turnOrder[0].GetComponent<Character>().attack;
             panelOfButtons.SetActive(false);
             // check for if anyone died
-            yield return new WaitForSeconds(1.25f);
-            checkDeath(battleCharacters);
-            // move turnOrder
-            GameObject playerTurn = turnOrder[0];
-            turnOrder.RemoveAt(0);
-            turnOrder.Add(playerTurn);
-            StartCoroutine(CPUTurn(turnOrder, battleCharacters));
+            yield return new WaitForSeconds(waitTime);
+            if (!checkDeath(battleCharacters)) {
+                // move turnOrder
+                GameObject playerTurn = turnOrder[0];
+                turnOrder.RemoveAt(0);
+                turnOrder.Add(playerTurn);
+                StartCoroutine(CPUTurn(turnOrder, battleCharacters));
+            }
+        } else if (waitForButton.PressedButton == runButton) {
+            int runRoll = RandomNumber(0,100);
+            panelOfButtons.SetActive(false);
+            if (runRoll <= runChance) {
+                battleText.text = turnOrder[0].GetComponent<Character>().characterName + " fled!";
+                yield return new WaitForSeconds(waitTime);
+                stateEnum.state = PlayerState.CurrentPlayerState.OVERWORLD;
+                StopAllCoroutines();
+            } else {
+                battleText.text = turnOrder[0].GetComponent<Character>().characterName + " tried to run, but couldn't get away!";
+                yield return new WaitForSeconds(waitTime);
+                // move turnOrder
+                GameObject playerTurn = turnOrder[0];
+                turnOrder.RemoveAt(0);
+                turnOrder.Add(playerTurn);
+                StartCoroutine(CPUTurn(turnOrder, battleCharacters));
+            }
         }
     }
 
@@ -103,12 +144,13 @@ public class BattleScript : MonoBehaviour
         battleText.text = turnOrder[0].GetComponent<Character>().characterName + " attacked for " + turnOrder[0].GetComponent<Character>().attack + " damage!";
         target.GetComponent<Character>().currentHP -= turnOrder[0].GetComponent<Character>().attack;
         // check for if anyone died
-        yield return new WaitForSeconds(1.25f);
-        checkDeath(battleCharacters);
-        // move turnOrder
-        GameObject playerTurn = turnOrder[0];
-        turnOrder.RemoveAt(0);
-        turnOrder.Add(playerTurn);
-        StartCoroutine(PlayerTurn(turnOrder, battleCharacters));
+        yield return new WaitForSeconds(waitTime);
+        if (!checkDeath(battleCharacters)) {
+            // move turnOrder
+            GameObject playerTurn = turnOrder[0];
+            turnOrder.RemoveAt(0);
+            turnOrder.Add(playerTurn);
+            StartCoroutine(PlayerTurn(turnOrder, battleCharacters));
+        }
     }
 }
--- Assets/Scripts/BattleTriggerScript.cs
+++ Assets/Scripts/BattleTriggerScript.cs
@@ -5,11 +5,12 @@ using UnityEngine;
 public class BattleTriggerScript : MonoBehaviour
 {
     public GameObject enemy;
+    public GameObject GameManager;
 
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Players") {
-            BattleScript.BattleTrigger(enemy);
+            GameManager.GetComponent<BattleScript>().BattleTrigger(enemy);
         }
     }
 }
