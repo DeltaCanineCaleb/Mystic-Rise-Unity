6a0f6c74c97f43d0394aa74e35cf881c521a9137
DeltaCanineCaleb <89848601+DeltaCanineCaleb@users.noreply.github.com>
2021-12-10 08:25:32 -0500

    Started on inventory gui (super distracted today)

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -17,15 +17,24 @@ public class Inventory : MonoBehaviour
     }
     #endregion
     
+    public delegate void OnItemChanged();
+    public OnItemChanged onItemChangedCallback;
+
     public List<Item> items = new List<Item>();
 
     public void Add(Item item)
     {
         items.Add(item);
+        if (onItemChangedCallback != null) {
+            onItemChangedCallback.Invoke();
+        }
     }
 
     public void Remove(Item item)
     {
         items.Remove(item);
+        if (onItemChangedCallback != null) {
+            onItemChangedCallback.Invoke();
+        }
     }
 }
